2287a9d3b726a1c1b5e3ec28f69e04d7cf45dd2c
Fatih Göktaş <fatih.goktas@overtech.com.tr>
2022-09-26 22:14:22 +0300

    First Lesson added

---
+++ flutter_complete_guide/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer chosen!');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      'What \'s your favorite color?',
+      'What \'s your favorite animal?'
+    ];
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('My First App!'),
+      ),
+      body: Column(
+        children: [
+          Text('The Questions!'),
+          ElevatedButton(
+            onPressed: answerQuestion,
+            child: Text('Answer 1'),
+          ),
+          ElevatedButton(
+            onPressed: answerQuestion,
+            child: Text('Answer 2'),
+          ),
+          ElevatedButton(
+            onPressed: answerQuestion,
+            child: Text('Answer 3'),
+          ),
+        ],
+      ),
+    ));
+  }
+}
