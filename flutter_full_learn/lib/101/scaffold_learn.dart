ada40883bb61db1b7734e342fa8853d0fec6dcaa
Fatih Göktaş <fatih.goktas@overtech.com.tr>
2022-10-12 19:38:42 +0300

    updated

--- flutter_full_learn/lib/101/scaffold_learn.dart
+++ flutter_full_learn/lib/101/scaffold_learn.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+//import './container_sized_box_learn.dart';
 
 class ScaffoldLearn extends StatelessWidget {
   const ScaffoldLearn({super.key});
@@ -10,23 +11,43 @@ class ScaffoldLearn extends StatelessWidget {
         title: const Center(child: Text('Scaffold Samples')),
       ),
       backgroundColor: Colors.red,
+      extendBody: true,
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              builder: (context) => (
+                    Container(
+                      height: 200,
+                    )
+          ));
+        },
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       drawer: const Drawer(),
-      bottomSheet: const Text('data'),
+      // bottomSheet: BottomSheet(
+      //   onClosing: () {
+
+      //   },
+      //   builder: (context) {
+      //     return const Text('data');
+      //   },
+      // ),
       body: const Text('Merhaba'),
-      bottomNavigationBar: BottomNavigationBar(items: const [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.abc_outlined),
-          label: 'a',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.abc_outlined),
-          label: 'b',
-        ),
-      ]),
+      bottomNavigationBar: SizedBox(
+        height: 100,
+       // decoration:ProjectContainerDecoration(),
+        child: BottomNavigationBar(items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.abc_outlined),
+            label: 'Start',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.abc_outlined),
+            label: 'Exit',
+          ),
+        ]),
+      ),
     );
   }
 }
